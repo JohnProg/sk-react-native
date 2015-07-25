978ef08e38c53aa2d53b3969ca679a2cbcacf8fb
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2015-07-25 17:25:04 +0100

    Add tracked artists view

--- index.ios.js
+++ index.ios.js
@@ -8,52 +8,128 @@ var React = require('react-native');
 var {
   AppRegistry,
   StyleSheet,
+  NavigatorIOS,
+  ListView,
   Text,
   View,
+  Image,
+  ActivityIndicatorIOS,
 } = React;
 
 var api = require('./songkick-api');
 
-api.getTrackedArtists('arnaud-rinquin').then(function(data){
-  console.log(data);
-});
-
-var songkick = React.createClass({
-  render: function() {
+class Songkick extends React.Component {
+  render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Songkick
-        </Text>
-        <Text style={styles.instructions}>
-          I am 3 minutes into trying React Native
-        </Text>
-        <Text style={styles.instructions}>
-          And I already go live reaload and chrome debugging
-        </Text>
+      <NavigatorIOS
+        style={styles.navigatorios}
+        initialRoute={{
+          component: Artists,
+          title: 'My tracked artists'
+        }}
+        tintColor="#7ed3f4"
+        barTintColor="#f80046"
+        titleTextColor="#ffffff"
+       />
+    );
+  }
+}
+
+class Artists extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      loaded: false,
+      dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id })
+    }
+
+    this.fetchArtists();
+  }
+
+  fetchArtists() {
+    api.getTrackedArtists('arnaud-rinquin').then((data) => this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(data.resultsPage.results.artist),
+      loaded: true
+    }))
+  }
+
+  renderArtist(artist){
+    return <Artist artist={artist}/>
+  }
+
+  renderLoading(){
+    return (
+      <View style={styles.centering} >
+        <ActivityIndicatorIOS color="#f80046" size="large"/>
       </View>
     );
   }
-});
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoading();
+    }
+
+    return (
+      <ListView
+        style={styles.artists}
+        dataSource={this.state.dataSource}
+        renderRow={this.renderArtist}
+      />
+    );
+  }
+}
+
+class Artist extends React.Component {
+  render() {
+    const {artist} = this.props
+    return (
+      <View style={styles.artist}>
+        <Image
+          style={styles.thumbnail}
+          source={ {uri: `https://images.sk-static.com/images/media/profile_images/artists/${artist.id}/large_avatar`} }
+        />
+        <Text style={styles.artistText}>{artist.displayName}</Text>
+      </View>
+    )
+  }
+}
+
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  thumbnail: {
+    width: 80,
+    height: 80,
+  },
+  artists: {
+    backgroundColor: '#F5FCFF',
+  },
+  artist: {
+    flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#F80046',
+    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 70,
-    textAlign: 'center',
-    margin: 10,
-    color: 'white',
+  artistText: {
+    flex: 1,
+    paddingLeft: 10,
+    fontSize: 20
   },
-  instructions: {
-    textAlign: 'center',
-    marginBottom: 5,
-    color: 'white',
+  navigatorios: {
+    flex: 1,
+    backgroundColor: '#1b1d24'
+  },
+  centering: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
   },
 });
 
-AppRegistry.registerComponent('songkick', () => songkick);
+AppRegistry.registerComponent('songkick', () => Songkick);
