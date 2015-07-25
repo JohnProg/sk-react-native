0f90d638ce41d173a25d67c9bab238a5f48df9c0
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2015-07-25 11:35:19 +0100

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var songkick = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Songkick
+        </Text>
+        <Text style={styles.instructions}>
+          I am 3 minutes into trying React Native
+        </Text>
+        <Text style={styles.instructions}>
+          And I already go live reaload and chrome debugging
+        </Text>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F80046',
+  },
+  welcome: {
+    fontSize: 70,
+    textAlign: 'center',
+    margin: 10,
+    color: 'white',
+  },
+  instructions: {
+    textAlign: 'center',
+    marginBottom: 5,
+    color: 'white',
+  },
+});
+
+AppRegistry.registerComponent('songkick', () => songkick);
